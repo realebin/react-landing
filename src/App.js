5fbce64baa4c0f5f3a966ebbbed0d01c12e8cbca
Febrina Anastasha <febrinafeb10@gmail.com>
2022-04-06 07:33:52 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { init } from "./utils/initDronesLayer.js"
+import { useEffect } from "react"
 
 function App() {
+  useEffect(() => {
+    init();
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="App container">
+      <div className="hero my-5">
+        <h1 className="title">
+          Misalkan Nama Barang
+        </h1>
+        <p className="text">
+        "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="row h-100 pb-5 pricing-table">
+        <div className="col pricing-card">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+              <a href="#" className="btn btn-primary mt-4">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div className="col pricing-card">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+              <a href="#" className="btn btn-primary mt-4">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div className="col pricing-card">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+              <a href="#" className="btn btn-primary mt-4">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
